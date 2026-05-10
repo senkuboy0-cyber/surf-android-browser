40e238174ec690c4ca1aa67a4a62591e3a691bae
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-10 16:53:11 +0600

    Add DateConverter for Room database date handling

---
+++ app/src/main/java/com/surf/browser/utils/DateConverter.kt
@@ -0,0 +1,17 @@
+package com.surf.browser.utils
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class DateConverter {
+    
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+    
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
